678caaa8d14d054bf43d0577810c0da721c73608
jchy <jaswantchaudhary@hotmail.com>
2021-09-22 22:59:59 +0530

    scope in js

---
+++ scope.js
@@ -0,0 +1,50 @@
+// var sum =10;
+// for(var i=0;i<10;i++)
+// {
+//     for(var j=0;j<20;j++)
+//     {
+//         sum=0;
+//     }
+// }
+// console.log(i,j);
+// console.log(sum);
+// function app(){
+//     console.log(x);
+// }
+// console.log("oop")
+// let x=100;
+//  x=1200;
+// app();
+
+// const a= 123;
+// a=25;
+
+// 
+// const obj = {
+//     name:"albert",
+//     address :{
+//         country: "Infia",
+//     pain: "ijdjijs"}
+// }
+// // Object.freeze(obj);
+// obj.name ="ndjdndj";
+// console.log(obj);
+
+// function rec(a){
+//     if(a>100){
+//         return a;
+//     }
+//     else {
+//         a=a+1;
+//         rec(a+1);
+//     }
+       
+// }
+// var a=0;
+// var b = rec(a);
+// console.log(b);
+var m=5;
+var n=3;
+var arr= new Array(n).fill(new Array(m).fill(0));
+arr[0][0]=1;
+console.log(arr);
